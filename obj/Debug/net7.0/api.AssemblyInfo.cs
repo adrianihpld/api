d97fd6fc2ac2369c8831073e158cd7c870a754b6
adrianihpld <139892026+adrianihpld@users.noreply.github.com>
2024-03-20 21:25:01 -0500

    Added Validation through Dtos

--- obj/Debug/net7.0/api.AssemblyInfo.cs
+++ obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3527614c37b50b310e110d7cc5c5dd0a419fc5d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b13ccfc71699a422d060a353295a214e724cc7b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
