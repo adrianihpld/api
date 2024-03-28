d1ca8a2a924f2d4922f74fee06f2ebf3b43a5bff
adrianihpld <139892026+adrianihpld@users.noreply.github.com>
2024-03-27 23:02:41 -0500

    Added additional packages for EI identity for login purposes

--- obj/Debug/net7.0/api.AssemblyInfo.cs
+++ obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e60d2ba5c340e4aded59c132888f4ea475c8edf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8b6010d26808f1aa121757c49487597db672ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
