3fcb791707b085d7e522473d8f9ffd463f81143a
adrianihpld <139892026+adrianihpld@users.noreply.github.com>
2024-04-09 22:22:48 -0500

    Login (Check notes to user info that was used to test login)

--- obj/Debug/net7.0/api.AssemblyInfo.cs
+++ obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8827344dc570e9d8d9168a8230fe150bf56e1671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40537aef3e2fd66163736ceb16a28f45f11b4a5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
